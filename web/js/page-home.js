f70a1ec7474a265e71ff2160840c468d6d3f7327
kmcgaire <kevinmcgaire@gmail.com>
2014-04-13 03:24:35 -0400

    Sample API call to get the summary data. Populate summary page dynamically

--- web/js/page-home.js
+++ web/js/page-home.js
@@ -4,5 +4,56 @@
 
 
 App.populator('home', function($page, data){
+  var $tabTemplate = $page.querySelector('.tab'),
+    username = data.username,
+    $tabParent = $tabTemplate.parentNode;
 
+  $tabTemplate.parentNode.removeChild($tabTemplate);
+
+  API.getSummaryData(username, function (users) {
+    console.log('test');
+    if(users){
+      users.forEach(function (user) {
+        console.log(JSON.stringify(user));
+        renderTab(user);
+      })
+    }
+  });
+
+
+
+  function renderTab(user){
+    var $tab = $tabTemplate.cloneNode(true);
+    var userPic = $tab.querySelector('.user-thumbnail');
+    if(user.thumbnail){
+      userPic.src = user.thumbnail;
+    } else {
+      userPic.src = "img/noprofile.png";
+    }
+    $tab.querySelector('#fullname').innerHTML = user.fullName;
+    $tab.querySelector('#username').innerHTML = user.username;
+    var owed = $tab.querySelector('#owed');
+    var text = $tab.querySelector('#text');
+    if (user.balance > 0){
+      text.innerHTML = 'owed to u';
+      text.classList.add('positive');
+      owed.classList.add('positive');
+    } else {
+      text.innerHTML = 'owing';
+      text.classList.add('negative');
+      owed.classList.add('negative');
+    }
+    owed.innerHTML = '$' + Number(user.balance).toFixed(2);
+
+    setTabClickable($tab, user.username);
+    $tabParent.appendChild($tab);
+  }
+
+  function setTabClickable($tabElement, username){
+    new Clickable($tabElement);
+    $tabElement.addEventListener('click', function () {
+      var data = {username: username}
+      App.load('detailedView', data);
+    });
+  }
 });
---
+++ web/zerver/API.js
@@ -0,0 +1,23 @@
+
+
+exports.getSummaryData = function (username, callback) {
+  var sampleData = [
+    {
+      username: 'kmcgaire',
+      fullName: 'Kevin McGaire',
+      thumbnail: null,
+      balance: -3.5
+    } , {
+      username: 'robinsharma',
+      fullName: 'Robin Sharma',
+      thumbnail: null,
+      balance: 2.5
+    } , {
+      username: 'kmcgaire',
+      fullName: 'Kevin McGaire',
+      thumbnail: null,
+      balance: 3
+    }
+  ];
+  callback(sampleData);
+}
