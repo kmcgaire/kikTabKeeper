76f92611d9740650a50880820065d4358e7e875d
kmcgaire <kevinmcgaire@gmail.com>
2014-05-07 13:40:09 -0400

    Implemented getSummaryData

--- web/zerver/API.js
+++ web/zerver/API.js
@@ -1,70 +1,102 @@
 
+
+var BackendSummary = require('./summary.js');
+
 exports.createNewContact = function (owner, contact, callback) {
   callback(true);
 };
 
 exports.getSummaryData = function (username, callback) {
-  var sampleData = [
-    {
-      username: 'kik_debug2',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: -3.5
-    } , {
-      username: 'robinsharma',
-      fullName: 'Robin Sharma',
-      thumbnail: null,
-      balance: 2.5
-    } , {
-      username: 'kmcgaire',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: 3
-    }, {
-      username: 'robinsharma',
-      fullName: 'Robin Sharma',
-      thumbnail: null,
-      balance: 2.5
-    } , {
-      username: 'kmcgaire',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: 3
-    }, {
-      username: 'robinsharma',
-      fullName: 'Robin Sharma',
-      thumbnail: null,
-      balance: 2.5
-    } , {
-      username: 'kmcgaire',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: 3
+  var dataToReturn =[];
+  BackendSummary.getUsersSummary(username, function (tabList) {
+    var dataToInsert;
+    if(tabList){
+      tabList.forEach(function (summaryTab) {
+        if (summaryTab.user1.username === username){
+          dataToInsert = {};
+          dataToInsert.username = summaryTab.user2.username;
+          dataToInsert.fullName = summaryTab.user2.fullName;
+          dataToInsert.thumbnail = summaryTab.user2.thumbnail;
+          dataToInsert.balance = -1 * summaryTab.amount;
+        } else if (summaryTab.user2.username === username){
+          dataToInsert = {};
+          dataToInsert.username = summaryTab.user1.username;
+          dataToInsert.fullName = summaryTab.user1.fullName;
+          dataToInsert.thumbnail = summaryTab.user1.thumbnail;
+          dataToInsert.balance = summaryTab.amount;
+        } else {
+          console.log('--------------------------------------------------------------------------------')
+          console.log('Returned an item from DB that should not have match query parameters')
+          console.log('--------------------------------------------------------------------------------')
+        }
+        if(dataToInsert){
+          dataToReturn.push(dataToInsert);
+        }
+      });
+      callback(dataToReturn);
     }
-    , {
-      username: 'robinsharma',
-      fullName: 'Robin Sharma',
-      thumbnail: null,
-      balance: 2.5
-    } , {
-      username: 'kmcgaire',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: 3
-    }
-    , {
-      username: 'robinsharma',
-      fullName: 'Robin Sharma',
-      thumbnail: null,
-      balance: 2.5
-    } , {
-      username: 'kmcgaire',
-      fullName: 'Kevin McGaire',
-      thumbnail: null,
-      balance: 3
-    }
-  ];
-  callback(sampleData);
+  });
+//  var sampleData = [
+//    {
+//      username: 'kik_debug2',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: -3.5
+//    } , {
+//      username: 'robinsharma',
+//      fullName: 'Robin Sharma',
+//      thumbnail: null,
+//      balance: 2.5
+//    } , {
+//      username: 'kmcgaire',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: 3
+//    }, {
+//      username: 'robinsharma',
+//      fullName: 'Robin Sharma',
+//      thumbnail: null,
+//      balance: 2.5
+//    } , {
+//      username: 'kmcgaire',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: 3
+//    }, {
+//      username: 'robinsharma',
+//      fullName: 'Robin Sharma',
+//      thumbnail: null,
+//      balance: 2.5
+//    } , {
+//      username: 'kmcgaire',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: 3
+//    }
+//    , {
+//      username: 'robinsharma',
+//      fullName: 'Robin Sharma',
+//      thumbnail: null,
+//      balance: 2.5
+//    } , {
+//      username: 'kmcgaire',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: 3
+//    }
+//    , {
+//      username: 'robinsharma',
+//      fullName: 'Robin Sharma',
+//      thumbnail: null,
+//      balance: 2.5
+//    } , {
+//      username: 'kmcgaire',
+//      fullName: 'Kevin McGaire',
+//      thumbnail: null,
+//      balance: 3
+//    }
+//  ];
+//  callback(sampleData);
 };
 
 exports.getTabList = function(owner, username, callback){
