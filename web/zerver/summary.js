5dbbd982ad91efae593ff1e5347cd2aa07476c90
kmcgaire <kevinmcgaire@gmail.com>
2014-05-08 10:13:47 -0400

    Fixed dat MongoDB query in Summary

--- web/zerver/summary.js
+++ web/zerver/summary.js
@@ -32,15 +32,15 @@ exports.getUsersSummary= function (username, callback) {
 };
 
 // user1 owes user 2 the amount
-exports.updateTabSummary = function (user1, user2, amount) {
+exports.updateTabSummary = function (username1, username2, amount) {
   Summary.update({
     $or: [
       {
-        user1: { username: user1 },
-        user2: { username: user2 }
+        'user1.username': username1 ,
+        'user2.username': username2
       }, {
-        user1: { username: user2 },
-        user2: { username: user1 }
+        'user1.username': username2 ,
+        'user2.username': username1
       }
     ]
   }, {
