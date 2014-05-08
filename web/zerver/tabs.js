5b0b2c49e689d0232fd243ecab4b798995524155
kmcgaire <kevinmcgaire@gmail.com>
2014-05-08 15:20:48 -0400

    Tab Collection Backend Implementation

---
+++ web/zerver/tabs.js
@@ -0,0 +1,36 @@
+/**
+ * Created by kevin on 5/8/2014.
+ */
+
+var Tabs = require('./db').collection('tabs');
+
+
+// user1 owes user2 the amount
+exports.insertTab = function (user1, user2, amount, currency, description, date, callback) {
+  var tabToInsert = {
+    user1: user1,
+    user2: user2,
+    amount: amount,
+    currency: currency,
+    description: description,
+    date: date
+  };
+  Tabs.insert(tabToInsert);
+  callback(tabToInsert);
+};
+
+exports.getTabs = function (username1, username2, callback) {
+  Tabs.find({
+    $or: [
+      {
+        'user1.username': username1 ,
+        'user2.username': username2
+      }, {
+        'user1.username': username2 ,
+        'user2.username': username1
+      }
+    ]
+  }).sort({ date: -1 }, function (err, tabList) {
+      callback(tabList)
+    });
+};
